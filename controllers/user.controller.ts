898a93d288327a55d4b665e57f8b8b3dae491e47
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-02 12:07:08 +0100

    add the user controller

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -5,7 +5,7 @@ import {IUser} from "../interfaces/User"
 import userModel from "../models/User.model"
 import { AppError, HttpCode } from "../utils/App.Error"
 
-
+// TO REGISTER
 export const Register = asyncHandler(
     async(req:Request<{},{},IUser>
         ,res:Response,next:NextFunction):Promise<Response>=>{
@@ -15,40 +15,60 @@ export const Register = asyncHandler(
         const hashedPassword :string=await bcrypt.hash(password,salt);
 
         const user = await userModel.create({
-            name,email,password:hashedPassword
+            name,email,password:hashedPassword,confirmPassword :password,
         })
 
         if(!user){
             next(
                 new AppError({
                     message:"Account not Found",
-                    httpCode:HttpCode.NOT_FOUND,
+                    httpCode:HttpCode.BAD_REQUEST,
                 })
             )
         }
 
-        return res.status(200).json({
+        return res.status(HttpCode.CREATED).json({
             user
         })
     }
 )
 
 
+// TO LOGIN
+export const Login = asyncHandler(
+    async(req:Request<{},{},IUser>
+        ,res:Response,next:NextFunction):Promise<Response>=>{
+        const {email,password} = req.body;
 
-// export const Login = asyncHandler(
-//     async(req:Request,res:Response,next:NextFunction):Promise<Response>=>{
-//         const {email,password} = req.body;
+        if(!email || !password){
+            next(
+                new AppError({
+                    message:"Please Provide the valid email or password",
+                    httpCode:HttpCode.BAD_REQUEST,
+                })
+            )
+        }
 
-//         const user = await userModel.findOne({email})
-//         if(!user){
-//             next(
-//                 new AppError({
-//                     message:"couldn't login user in ",
-//                     httpCode:HttpCode.FORBIDDEN,
-//                 })
-//             )
-//         }
-//         const checkPass = await bcrypt.compare(password,user!password)
-//         if(!user)
-//     }
-// )
+        const user = await userModel.findOne({email})
+        if(!user){
+            next(
+                new AppError({
+                    message:"couldn't login user in ",
+                    httpCode:HttpCode.FORBIDDEN,
+                })
+            )
+        }
+        const checkPass = await bcrypt.compare(password,user!.password)
+        if(!user){
+            next(
+                new AppError({
+                    message:"Couldn't Login",
+                    httpCode:HttpCode.NOT_FOUND,
+                })
+            )
+        }
+        return res.status(HttpCode.OK).json({
+            user
+        })
+    }
+)
