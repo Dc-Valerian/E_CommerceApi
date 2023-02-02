b00e74efea2bbbb47367a64da55a7746a7e1003c
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-02 15:17:53 +0100

    updates

--- src/app.ts
+++ src/app.ts
@@ -9,18 +9,19 @@ import router from "../routes/user.router"
 export const appConfig=(app:Application)=>{
     app.use(morgan("dev")).use(express.json()).use(cors()).use('/api/auth',router)
 
+        .use("/api/user",router)
     // CATCH WRONG ROUTES
     app.all("*",(req:Request,res:Response,next:NextFunction)=>{
         next(
             new AppError({
-                message:`This Route ${req.originalUrl} does not exist `,
+                message:`This Route ${req.originalUrl} does not exists `,
                 httpCode:HttpCode.NOT_FOUND
             })
         )
 
     })
 
-    .use("./api/user",router)
+
     // ERROR MIDDLEWARES
     app.use(errorHandler)
 }
