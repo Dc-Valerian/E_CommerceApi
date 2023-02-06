afe8e122ccaa9445fe22932e7a727a19b0318e34
Dc-Valerian <valerianpedro03@gmail.com>
2023-02-06 18:55:23 +0100

    New Changes

--- middlewares/authorization/admin.auth.ts
+++ middlewares/authorization/admin.auth.ts
@@ -1,10 +1,20 @@
 import { Request,Response,NextFunction } from "express";
 import { IAuthUser } from "../../interfaces/User";
+import { AppError, HttpCode } from "../../utils/App.Error";
 
-export const isAdmin = async(req:IAuthUser,res:Response,next:NextFunction)=>{
+export const isAdmin = (req:IAuthUser,res:Response,next:NextFunction)=>{
     const user = req!.user
 
-    if(user.role === "admin"){
-        
+    const adminUser = user && user.role === "admin"
+
+    if(!adminUser){
+        next(
+            new AppError({
+                message:"Unauthorized admin user",
+                httpCode:HttpCode.UNAUTHORIZED
+            })
+        )
     }
+
+    next()
 }
